10f6ed6d6f2c8f887c189e1bd59643afdc200a05
jlb1999 <jlbroughton88@gmail.com>
2019-12-12 14:39:48 -0500

    fixed file name

---
+++ client/build/precache-manifest.267b33aea73151b24e68dd680fbbaa02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6e4bee76837b75a88ab2ec8f2382e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37896c82db502cbd34bc",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6fa795f2ae33a196c37f",
+    "url": "/static/js/2.537e50ca.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.537e50ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37896c82db502cbd34bc",
+    "url": "/static/js/main.7543240b.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
